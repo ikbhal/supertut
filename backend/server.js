adf2cbf05f03e95a0e35f40f84986038d9c095e3
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-09-04 10:30:14 +0530

    first server commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 7000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ping', (req, res) => {
+  res.send('Pong!');
+});
+
+app.post('/data', (req, res) => {
+  const requestData = req.body;
+  console.log('Received JSON data:', requestData);
+  res.json({ message: 'Data received successfully' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
