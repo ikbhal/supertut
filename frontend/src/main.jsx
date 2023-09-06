4f0c4cfa944673d7ff29c5292c6e6320a80d6b93
Kishore Ch <kishu.559@gmail.com>
2023-09-06 09:12:43 +0530

    user authentication added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+  domain="dev-io8yt1yy2xpcnykz.us.auth0.com"
+  clientId="QLYTupAq6BQB8fVrudUyTlnRK54YUMBh"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  <App />
+</Auth0Provider>,
 )
+
