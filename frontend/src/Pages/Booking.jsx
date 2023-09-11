7ed143bbbf0610b225db0ddd2f0b3119c31dc90a
Kishore Ch <kishu.559@gmail.com>
2023-09-11 23:23:06 +0530

    booking page btn

--- frontend/src/Pages/Booking.jsx
+++ frontend/src/Pages/Booking.jsx
@@ -28,12 +28,60 @@ const Booking = () => {
           <div>
             <h3>Who is this lesson for?</h3>
             <div className="select-person">
-                <input className="input" id="you" type="radio" name="radio" value="you" />
-                <label className="label" htmlFor="you">You</label>
-                <input className="input" id="others" type="radio" name="radio" value="someoneElse" />
-                <label className="label" htmlFor="others">Someone Else</label>
-
+              <input
+                className="input"
+                id="you"
+                type="radio"
+                name="radio"
+                checked="true"
+                value="you"
+              />
+              <label className="label" htmlFor="you">
+                You
+              </label>
+              <input
+                className="input"
+                id="others"
+                type="radio"
+                name="radio"
+                value="someoneElse"
+              />
+              <label className="label" htmlFor="others">
+                Someone Else
+              </label>
             </div>
+          </div>
+          <div>
+            <h3>Date of first class</h3>
+            <div className="select-date select-person">
+              <input
+                id="now"
+                type="radio"
+                name="radio1"
+                checked="true"
+                value="now"
+              />
+              <label htmlFor="now">As soon as possible</label>
+              <input id="date" type="radio" name="radio1" value="date" />
+              <label htmlFor="date">Suggest a Date</label>
+            </div>
+          </div>
+          <div>
+            <h3>Contact information</h3>
+            <p>
+              This information will only be communicated to the teachers you
+              select.
+            </p>
+          </div>
+          <div className="contact-container">
+          <div className="contact-info">
+            <label htmlFor="address">Address</label>
+            <input id="address" type="text" />
+          </div>
+          <div className="phone-number contact-info">
+            <label htmlFor="phone">Phone Number</label>
+            <input id="phone" type="text" />
+          </div>
           </div>
         </div>
       </div>
